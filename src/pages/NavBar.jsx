6d930e8684aeaf0b0d0c4c64e73e33cd32ebfa6c
JotaC <jota.crow@gmail.com>
2024-03-01 00:00:15 -0500

    Add programs and tools

---
+++ src/pages/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import home from './home'
+
+const NavBar = () => {
+    return (
+        <div>
+            <home.jsx/>
+        </div>
+    )
+}
+
+export default NavBar

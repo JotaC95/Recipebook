fec6a7af8c04d11de294af1a4066704cf6326511
JotaC <jota.crow@gmail.com>
2024-03-07 00:08:16 -0500

    Home page

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import Favoritos from './Favoritos';
+import Products from './Products';
 
-const title = 'grid justify-items-center content-center text-3xl h-60 m-20 bg-red-300 w-full';
-const search ='grid justify-items-center content-center text-3xl h-20 bg-white';
+
+const title = 'grid justify-items-center content-center gap-y-10 text-3xl h-80 bg-red-600 text-white';
+const search ='grid justify-items-center content-center text-3xl h-20 bg-white mt-20 mb-10';
 const Header = () => {
     return (
         <>
             <section className={title}>
-            <img className='h-80' src='src/img/logoserena.png' alt='logo-empresa'/>
-            <h1 >Recetario La Serena</h1>
+                <img className='h-56' src='src/img/logoserena.png' alt='logo-empresa'/>
+                <h1 >Recetario La Serena</h1>
             </section>
-            <section className={search}>Search</section>
-
+            <Favoritos/>
+            <section className={search}>
+                <input className='border-4 border-red-600 w-42 text-center' placeholder='Buscar' type="text"/>
+            </section>
+            <Products/>
 
         </>
     )
